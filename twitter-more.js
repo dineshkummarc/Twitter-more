5af4286e4369e3039fd86ba4d121e0f42f0368a9
Florian Cargoët <florian.cargoet+github@centrale-marseille.fr>
2012-01-17 14:25:40 +0100

    added a "Favorites" link

--- twitter-more.js
+++ twitter-more.js
@@ -1,4 +1,7 @@
 /*global $:true */
+
+// go to last unread tweet link
+// placed on the right of the top toolbar
 $('<a href="#" class="go-to-last">Last</a>')
     .click(function(){
         var lastOffset=$('.last-new-tweet').offset();
@@ -9,3 +12,8 @@ $('<a href="#" class="go-to-last">Last</a>')
         }
     })
     .appendTo('.container');
+
+// go to favorites link
+// placed on the right of the 'Discover' link
+$('<li><a class="js-hover" href="/#!/favorites">Favorites</a></li>')
+    .appendTo('#global-actions');
