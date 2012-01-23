4cac9f2b6c21d897d0ea56ce48b417520ab3c3d3
Florian Cargoët <florian.cargoet+github@centrale-marseille.fr>
2012-01-23 09:47:31 +0100

    added a link to your Lists

--- twitter-more.js
+++ twitter-more.js
@@ -4,16 +4,20 @@
 // placed on the right of the top toolbar
 $('<a href="#" class="go-to-last"><i></i></a>')
     .click(function(){
-        var lastOffset=$('.last-new-tweet').offset();
+        var lastOffset = $('.last-new-tweet').offset();
         if(lastOffset){
             $('html,body').animate({
-                scrollTop : lastOffset.top-50
+                scrollTop : lastOffset.top - 50
             },500);
         }
     })
     .appendTo('.container');
 
-// go to favorites link
+// favorites link
 // placed on the right of the 'Discover' link
 $('<li><a class="js-hover" href="/#!/favorites">Favorites</a></li>')
+    .appendTo('#global-actions');
+
+// lists link
+$('<li><a class="js-hover" href="/#!/lists">Lists</a></li>')
     .appendTo('#global-actions');
